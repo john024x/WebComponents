3529eafaff20aab962869aa28d6e60fd4a26bf6c
Juan Fuentes <juan@2806-106e-0023-b91b-0000-0000-0000-000f.ipv6.infinitum.net.mx>
2021-04-08 19:18:35 -0500

    Basic creation of custom element

---
+++ index.js
@@ -0,0 +1,27 @@
+const template = document.createElement("div");
+template.innerHTML = `
+        <style>
+        .texto{
+            color: firebrick;
+        }
+            p{
+                color:blue;
+            }
+        </style>
+        <p class ="texto">This app is abaut </p>
+        <p>the history of an Avocado</p>
+    `
+//definicion de la clase
+class myElement extends HTMLElement{//el HTMLElement es necesario para la creacion de los custom Elements
+    constructor(){
+        super();
+        this.p = document.createElement("p");
+    }
+    connectedCallback(){
+        this.p.textContent = "Made with love by @John024x!";
+        this.appendChild(this.p);
+        this.appendChild(template);
+    }
+}
+customElements.define('happy-hacking',myElement);
+
