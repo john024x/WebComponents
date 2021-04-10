105bfee009acf256bb378d7dbb77fe713d2f7f95
Juan Fuentes <juan@2806-106e-0023-b91b-0000-0000-0000-000f.ipv6.infinitum.net.mx>
2021-04-09 21:00:11 -0500

    Use of custom text inside a custom element

---
+++ Slot/slot.js
@@ -0,0 +1,36 @@
+//definicion de la clase
+class myElement extends HTMLElement{//el HTMLElement es necesario para la creacion de los custom Elements
+    constructor(){
+        super();
+        this.attachShadow({mode: 'open'});//el nodo tiene que estar abierto para que pueda interactuar con los demas elementos
+    }
+    getTemplate(){
+        const template = document.createElement("template");
+        template.innerHTML = `
+            <section>
+                <h2>
+                    <slot></slot>
+                </h2>
+            </section>
+            ${this.GetStyle()}
+        `;
+        return template;
+    }
+    GetStyle(){
+        return `
+            <style>
+                h2{
+                    color: red;
+                }
+            </style>
+        `;
+    }
+    render(){
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));//en esta ocasión se renderiza diferente por el hecho de incluir el 'ShadowRoot'
+    }
+    connectedCallback(){
+        this.render();
+    }
+}
+customElements.define('happy-hacking',myElement);
+
